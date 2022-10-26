fc0d10edd9373bcf1aebbb0e189cc89c79e2bf1e
samuel cordova <cordovas1337@gmail.com>
2022-10-26 16:30:34 -0500

    added the starter code for updateLargeCup, can count total cups and cups that are full

--- script.js
+++ script.js
@@ -3,33 +3,42 @@ const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remainder = document.getElementById('remaining')
 
-smallCups.forEach((cup, index) => {
 
-    cup.addEventListener('click', ( )=> highlightCups(index))
+updateLargeCup(); 
+smallCups.forEach((cup, index) => {
+    cup.addEventListener('click', () => highlightCups(index))
     console.log(index)
 })
 
 function highlightCups(index) {
-    if(smallCups[index].classList.contains('full') && 
-    /*
-    if the next index doesn't contain full class toggle on and off by
-    decrementing current index by 1
-     */ 
-    !smallCups[index].nextElementSibling.classList.contains('full')) {
+    if (smallCups[index].classList.contains('full') &&
+        /*
+        if the next index doesn't contain full class toggle on and off by
+        decrementing current index by 1
+         */
+        !smallCups[index].nextElementSibling.classList.contains('full')) {
         index--
     }
+
     /*
     A for each loop to loop through all the cups and add the class to 
     the appropriate cups.
-     */ 
+     */
     smallCups.forEach((cup, index2) => {
-        if(index2 <= index) {
+        if (index2 <= index) {
             cup.classList.add('full')
         }
         else {
             cup.classList.remove('full')
         }
-    }) 
+    })
+    updateLargeCup()
+}
 
-        
-    }
+function updateLargeCup() {
+    // furst see how many full cups there are
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCups = smallCups.length
+    console.log(totalCups)
+    console.log(fullCups)
+}
